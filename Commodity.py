43a48ae8430d109d6741fb408d3ab04545709048
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-12 17:44:42 +0100

    Update Commodity.py

--- Commodity.py
+++ Commodity.py
@@ -3,10 +3,11 @@ from Describable import Describable
 
 class Commodity(Describable):
 
-    def __init__(self, group: CommodityGroup, id: int, name: str):
+    def __init__(self,  name: str,group: CommodityGroup, id: int):
         self.id = id
-        self.group = group
         self.__name = name
+        self.group = group
+        
 
     def describe(self):
-        return f'Commodity : {self.__name} ({self.group.name})'
+        return "Commodity :" + self.__name + self.group.name
