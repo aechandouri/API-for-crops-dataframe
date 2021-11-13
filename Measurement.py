1af4c7dad16ab675fd228c99fd2a14e90b8d3e49
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-13 11:10:16 +0100

    Update Measurement.py

--- Measurement.py
+++ Measurement.py
@@ -1,15 +1,18 @@
-from Commodity import Commodity
 from Indicator import Indicator
 from Describable import Describable
+from Commodity import Commodity
 
 class Measurement(Describable):
-    def __init__ (self, id: int, year : int, value : float, timeperiodId : int, timeperiodDescr : str, commodity : Commodity, indicator : Indicator):
+    def __init__ (self,  year : int,id: int, timeperiodDescr : str,value : float, timeperiodId : int, commodity : Commodity, indicator : Indicator):
+ 
+
+        self.__timeperiodId = timeperiodId
         self.__year = year
         self.__value = value
-        self.__timeperiodId = timeperiodId
         self.__timeperiodDescr = timeperiodDescr
+        
         self.commodity = commodity
         self.indicator = indicator
 
     def describe(self):
-        return f'_____________________________\n{self.__timeperiodDescr} de l\'année {self.__year} \n\t{self.commodity.describe()}\n\t{self.indicator.describe()}\n\tValeur : {self.__value} ({self.indicator.unit.describe()})'
+        return ("------------" \n + self.__timeperiodDescr+ self.__year \n + self.commodity.describe() \n + self.indicator.describe() \n + "Valeur =" + self.__value + self.indicator.unit.describe())
