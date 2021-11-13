79c8dc90a59272572bca54572eac86c2fe56c6bf
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-13 10:17:24 +0100

    Update IndicatorGroup.py

--- IndicatorGroup.py
+++ IndicatorGroup.py
@@ -1,10 +1,16 @@
 from enum import Enum
 
 class IndicatorGroup(Enum):
-    EXPORTS_AND_IMPORTS = 3
+    
     SUPPLY_AND_USE = 2
+    
     PRICES = 1
-    FEED_PRICE_RATIOS = 7
-    QUANTITIES_FED = 6
-    TRANSPORTATION = 4
+    
+    EXPORTS_AND_IMPORTS = 3
+    
     ANIMAL_UNIT_INDEXES = 5
+    FEED_PRICE_RATIOS = 7
+    
+    QUANTITIES_FED = 6
+    
+    TRANSPORTATION = 4
