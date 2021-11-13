35817a7b044f409c24df458e017ce0d07311d70f
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-13 10:16:01 +0100

    Update Indicator.py

--- Indicator.py
+++ Indicator.py
@@ -1,17 +1,17 @@
-from IndicatorGroup import IndicatorGroup
 from Unit import Unit
+from IndicatorGroup import IndicatorGroup
 from Describable import Describable
 
 class Indicator(Describable):
 
-    def __init__(self, id: int, frequency: int, freqDesc: str, geogLocation: str, indicatorGroup: IndicatorGroup, unit: Unit, name: str):
+    def __init__(self, id: int, freqDesc: str, geogLocation: str, ,frequency: int, unit: Unit, geogLocation: str, name: str,indicatorGroup: IndicatorGroup):
         self.id = id
-        self.unit = unit
         self.indicatorGroup = indicatorGroup
+        self.unit = unit
+        self.__geogLocation = geogLocation
         self.__frequency = frequency
         self.__frequencyDesc = freqDesc
-        self.__geogLocation = geogLocation
         self.name = name
 
     def describe(self):
-        return f'Geography : {self.__geogLocation}\n\tIndicator : {self.name} ({self.indicatorGroup.name})'
+        return ("Geography :"+ self.__geogLocation \n + "Indicator :" + self.name + self.indicatorGroup.name)
