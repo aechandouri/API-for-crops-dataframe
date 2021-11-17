99188a9183fdf5ca29c20149df4634b58acce4ec
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-17 10:09:21 +0100

    Update FoodCropsDataset.py

--- FoodCropsDataset.py
+++ FoodCropsDataset.py
@@ -1,24 +1,21 @@
-from FoodCropFactory import FoodCropFactory
+import pandas as pd
 from CommodityGroup import CommodityGroup
 from IndicatorGroup import IndicatorGroup
+from FoodCropFactory import FoodCropFactory
 from Unit import Unit
-import pandas
 
 class FoodCropsDataset:
     def __init__(self, factory: FoodCropFactory):
         self.factory = factory
 
+
+        self.__locationMeasurementIndex = {}
         self.__commodityMeasurementIndex = {}
         self.__indicatorGroupMeasurementIndex = {}
-        self.__locationMeasurementIndex = {}
         self.__unitMeasurementIndex = {}
 
         self.__measurements = []
 
-    def load(self, datasetPath: str):
-        dataframe = pandas.read_csv(datasetPath)
-        dataframe = dataframe.dropna()
-        dataframe.reset_index(drop=True)
 
         units = {
             1: self.factory.createCount(1, "Million bushels", "Million bushels"),
@@ -45,78 +42,84 @@ class FoodCropsDataset:
             45: self.factory.createUnitRatio(45, self.factory.createWeight(3, 1, "Metric tons"), self.factory.createSurface(1010, "hectare"), "Metric tons per hectare"),
             46: self.factory.createCount(46, "Million animal units", "Million animal units"),
         }
-
-        for index, row in dataframe.iterrows():
-            # Récupération du groupe de l'indicateur
-            indicatorGroup = IndicatorGroup(row["SC_Group_ID"])
-            # Création/récupération de l'unité de l'indicateur
-            id = row["SC_Unit_ID"]
+    def load(self, datasetPath: str):
+        FCdataset = pd.read_csv(datasetPath)
+        FCdataset = FCdataset.dropna()
+        FCdataset.reset_index(drop=True)
+        for i, j in FCdataset.iterrows():
+            # on récupère le grpe de l'indicateur
+            indicatorGroup = IndicatorGroup(j["SC_Group_ID"])
+            # On cree / récupère  l'unité de l'ind
+            id = j["SC_Unit_ID"]
             unit = units[id]
 
-            # Création/récupération de l'indicateur
-            indicator = self.factory.createIndicator(row["SC_Attribute_ID"], row["SC_Frequency_ID"], row["SC_Frequency_Desc"], row["SC_GeographyIndented_Desc"], indicatorGroup, unit, row["SC_Attribute_Desc"])
+            # On cree / récupère l'ind
+            ind = self.factory.createIndicator(j["SC_Attribute_ID"], j["SC_Frequency_ID"], j["SC_Frequency_Desc"], j["SC_GeographyIndented_Desc"], indicatorGroup, unit, j["SC_Attribute_Desc"])
 
-            # Récupération de groupe des cultures vivières
-            commodityGroup = CommodityGroup(row["SC_GroupCommod_ID"])
-            # Crétion/récupération de la culture vivière
-            commodity = self.factory.createCommodity(commodityGroup, row["SC_Commodity_ID"], row["SC_Commodity_Desc"])
+            # on récupère le grpe des cultures vivrières
+            commodityGroup = CommodityGroup(j["SC_GroupCommod_ID"])
+            
+            com = self.factory.createCommodity(commodityGroup, j["SC_Commodity_ID"], j["SC_Commodity_Desc"])
 
-            # Création de la mesure
-            measurement = self.factory.createMeasurement(index, row["Year_ID"], row["Amount"], row["Timeperiod_ID"], row["Timeperiod_Desc"], commodity, indicator)
+            # On créée ensuite la mesure
+            measure = self.factory.createMeasurement(i, j["Year_ID"], j["Amount"], j["Timeperiod_ID"], j["Timeperiod_Desc"], commodity, indicator)
             self.__measurements.append(measurement)
-
-            if commodityGroup in self.__commodityMeasurementIndex:
-                self.__commodityMeasurementIndex[commodityGroup].append(measurement)
+            
+            if j["SC_GeographyIndented_Desc"] in self.__locationMeasurementIndex:
+                self.__locationMeasurementIndex[j["SC_GeographyIndented_Desc"]].append(measurement)
             else:
-                self.__commodityMeasurementIndex[commodityGroup] = [measurement]
-
-            if indicatorGroup in self.__indicatorGroupMeasurementIndex:
-                self.__indicatorGroupMeasurementIndex[indicatorGroup].append(measurement)
-            else:
-                self.__indicatorGroupMeasurementIndex[indicatorGroup] = [measurement]
-
-            if row["SC_GeographyIndented_Desc"] in self.__locationMeasurementIndex:
-                self.__locationMeasurementIndex[row["SC_GeographyIndented_Desc"]].append(measurement)
-            else:
-                self.__locationMeasurementIndex[row["SC_GeographyIndented_Desc"]] = [measurement]
+                self.__locationMeasurementIndex[j["SC_GeographyIndented_Desc"]] = [measurement]
 
             if unit in self.__unitMeasurementIndex:
                 self.__unitMeasurementIndex[unit].append(measurement)
             else:
                 self.__unitMeasurementIndex[unit] = [measurement]
+                
+            if indicatorGroup in self.__indicatorGroupMeasurementIndex:
+                self.__indicatorGroupMeasurementIndex[indicatorGroup].append(measurement)
+            else:
+                self.__indicatorGroupMeasurementIndex[indicatorGroup] = [measurement]
+                
+            if commodityGroup in self.__commodityMeasurementIndex:
+                self.__commodityMeasurementIndex[commodityGroup].append(measurement)
+            else:
+                self.__commodityMeasurementIndex[commodityGroup] = [measurement]
 
-    def findMeasurements(self, commodityGroup: CommodityGroup = None, indicatorGroup: IndicatorGroup = None, geographicalLocation: str = None, unit: Unit = None) -> list:
-        measurementList = self.__measurements[:]
-        if commodityGroup is not None:
-            new_list = []
-            l = self.__commodityMeasurementIndex.get(commodityGroup, [])
-            for measure in measurementList:
-                if measure in l:
-                    new_list.append(measure)
-            measurementList = new_list
+            
 
-        if indicatorGroup is not None:
-            new_list = []
-            l = self.__indicatorGroupMeasurementIndex.get(indicatorGroup, [])
-            for measure in measurementList:
-                if measure in l:
-                    new_list.append(measure)
-            measurementList = new_list
+    def findMeasurements(self, geographicalLocation: str = None, commodityGroup: CommodityGroup = None, indicatorGroup: IndicatorGroup = None, unit: Unit = None) -> list:
+        list_measure = self.__measurements[:]
+        
 
         if geographicalLocation is not None:
-            new_list = []
-            l = self.__locationMeasurementIndex.get(geographicalLocation, [])
-            for measure in measurementList:
-                if measure in l:
-                    new_list.append(measure)
-            measurementList = new_list
+            liste = []
+            temp = self.__locationMeasurementIndex.get(geographicalLocation, [])
+            for val in list_measure:
+                if val in temp:
+                    liste.append(val)
+            list_measure = liste
+            
+        if commodityGroup is not None:
+            liste = []
+            temp = self.__commodityMeasurementIndex.get(commodityGroup, [])
+            for measure in list_measure:
+                if val in temp:
+                    list.append(val)
+            list_measure = liste
 
+        if indicatorGroup is not None:
+            liste = []
+            temp = self.__indicatorGroupMeasurementIndex.get(indicatorGroup, [])
+            for val in list_measure:
+                if val in temp:
+                    list.append(val)
+            list_measure = liste
         if unit is not None:
-            new_list = []
-            l = self.__unitMeasurementIndex.get(unit, [])
-            for measure in measurementList:
-                if measure in l:
-                    new_list.append(measure)
-            measurementList = new_list
+            liste = []
+            temp = self.__unitMeasurementIndex.get(unit, [])
+            for val in measurementList:
+                if val in temp:
+                    list.append(val)
+            list_measure = listew
 
-        return measurementList
+        return list_measure
