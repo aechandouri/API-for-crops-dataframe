2cf5dbcce344ebd1b0b2bffaaae54097b5409522
ECHANDOURI Abdessamad <74980253+aechandouri@users.noreply.github.com>
2021-11-18 13:39:37 +0100

    Update and rename Describable.py to Describable(with tkinter).py

---
+++ Describable(with tkinter).py
@@ -0,0 +1,31 @@
+
+import tkinter as tnk
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+
+class Describable:
+    def __init__(self):
+        #Main window
+        win_main = tnk()
+        win_main.title("Food crops dataset")
+        win_main.configure()
+      
+        #set window size
+        widthwin = win_main.winfo_screenwidth()
+        heightwin = win_main.winfo_screenheight()
+
+       
+        API_title = Label(main, text="FoodCorps API")
+        API_title.pack()
+
+        win_main.mainloop()
+
+
+    def describe(self) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    Describable()
